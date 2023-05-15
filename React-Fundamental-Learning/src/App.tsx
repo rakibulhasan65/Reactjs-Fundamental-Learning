fc911fbb19222270e7600e2ff4aa6028cccae0d7
rakibulhasan65 <rakibulhasan392364@gmail.com>
2023-05-15 17:27:02 +0600

    React App Install To Tailwindcss

---
+++ React-Fundamental-Learning/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-gray-900 text-white text-center p-5 font-sans font-bold">
+        Hello Bangladesh
+      </h1>
+    </>
+  );
+}
+
+export default App;
