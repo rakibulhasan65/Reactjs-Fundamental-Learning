1a743447acc0e74bd362adf8de521d084a8205c6
rakibulhasan65 <rakibulhasan392364@gmail.com>
2023-05-17 11:41:29 +0600

    Header Component Added

--- React-Fundamental-Learning/src/App.tsx
+++ React-Fundamental-Learning/src/App.tsx
@@ -1,16 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-  return (
-    <>
-      <h1 className="bg-gray-900 text-white text-center p-5 font-sans font-bold">
-        Hello Bangladesh
-      </h1>
-    </>
-  );
+  return <Header />;
 }
 
 export default App;
